91554c7209c266c3a0eb4bb2928c6ff221db9b59
Geeky-5hubham <81162491+Geeky-5hubham@users.noreply.github.com>
2023-01-20 15:25:05 +0530

    Time: 3 ms (89.74%), Space: 9.6 MB (55.94%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        
         int row = matrix.size();
         int col = matrix[0].size();
-        
+        /* Better than bruteforce
         int i = 0;
         int j = col-1;
         
@@ -11,6 +13,20 @@ public:
             if(matrix[i][j] == target)  return true;
             else if (matrix[i][j] < target)     i++;
             else    j--;
+        }
+        return false; */
+        int s=0;
+        int e=row*col-1;
+        int mid;
+        
+        while(s<=e){
+            mid = s+(e-s)/2;
+            int i = mid/col;
+            int j = mid%col;
+            
+            if(matrix[i][j] == target)  return true;
+            else if(matrix[i][j] > target)  e = mid-1;
+            else    s=mid+1;
         }
         return false;
     }
