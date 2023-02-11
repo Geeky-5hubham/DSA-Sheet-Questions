b1c9a1715659ecf77e72929abdcab4a65e9a8be7
Geeky-5hubham <81162491+Geeky-5hubham@users.noreply.github.com>
2023-02-12 02:41:16 +0530

    Time: 3 ms (49.80%), Space: 6 MB (44.43%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -2,17 +2,41 @@ class Solution {
 public:
     // Ans depends on number of nodes not values coz all numbersa are in increasing order and distinct  Ans for i(th) node= left sub tree Ans * right sub tree Ans = Ans(i-1)*Ans(n-i)
     
-    int numTrees(int n) {
-        if(n <= 1)
-            return 1;
+    int solve_Tabulation(int n){
+        vector<int> dp(n+1);
         
-        int ans = 0;
-        
-        // consider i as root node
-        for(int i=1;i<=n;i++){
-            ans += numTrees(i-1)*numTrees(n-i);
+        dp[0] = dp[1] = 1;
+
+        // i -> no. of nodes
+        for(int i=2;i<=n;i++){
+           // j -> root node
+            for(int j=1;j<=i;j++){
+                dp[i] += dp[j-1]*dp[i-j];
+            }
         }
         
-        return ans;
+        return dp[n];
+        
+    }
+    
+    int solve_memoisation(int n,vector<int> &dp){
+        
+        if(n <=1)
+            return 1;
+        
+        if(dp[n] != -1)
+            return dp[n];
+        
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            ans += solve_memoisation(i-1,dp)*solve_memoisation(n-i,dp);
+        }
+        
+        return dp[n] = ans;
+    }
+    
+    int numTrees(int n) {
+        vector<int> dp(n+1,-1);
+        return solve_memoisation(n,dp);
     }
 };
