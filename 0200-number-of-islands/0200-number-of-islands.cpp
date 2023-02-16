9b60ccfe790fefa4d143cc718cd50233ea1f5c32
Geeky-5hubham <81162491+Geeky-5hubham@users.noreply.github.com>
2023-02-17 05:14:40 +0530

    Time: 45 ms (42.41%), Space: 19.2 MB (13.32%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,57 @@
+class Solution {
+private:
+   void bfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>>&grid) {
+      // mark it visited
+      vis[row][col] = 1; 
+      queue<pair<int,int>> q;
+      // push the node in queue
+      q.push({row, col}); 
+      int n = grid.size(); 
+      int m = grid[0].size(); 
+      
+      // until the queue becomes empty
+      while(!q.empty()) {
+          int row = q.front().first; 
+          int col = q.front().second; 
+          q.pop(); 
+          
+          // traverse in the neighbours and mark them if its a land 
+          for(int i=-1;i<=1;i++)
+          {
+              int newr = row+i;
+              int newc = col+i;
+              
+              if(newr>=0 && newr<n && !vis[newr][col] && grid[newr][col]=='1')
+              {
+                  vis[newr][col] =true;
+                  q.push({newr,col});
+              }
+               if(newc>=0 && newc<m && !vis[row][newc] && grid[row][newc]=='1')
+              {
+                  vis[row][newc] =true;
+                  q.push({row,newc});
+              }
+          }
+      }
+  }
+    
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>> vis(n,vector<int>(m,0)) ;    //visitedMatrix;
+        
+        int cnt = 0;
+        
+        for(int row=0;row<n;row++){
+            for(int col=0;col<m;col++){
+                if(!vis[row][col] && grid[row][col] == '1'){
+                    cnt++;
+                    bfs(row,col,vis,grid);
+                }
+            }
+        }
+        return cnt;
+    }
+};
