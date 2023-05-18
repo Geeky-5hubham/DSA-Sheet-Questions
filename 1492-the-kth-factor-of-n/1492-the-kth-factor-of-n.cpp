2e2298079910583b6127fe27d029c1ed86a3be7f
Geeky-5hubham <81162491+Geeky-5hubham@users.noreply.github.com>
2023-05-18 23:20:06 +0530

    Time: 4 ms (40.20%), Space: 5.9 MB (96.35%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -3,15 +3,23 @@ public:
     int kthFactor(int n, int k) {
         int c = 0;
         
-        for(int i=1;i<=n;i++){
+        for(int i=1;i*i<n;i++){
             if(n%i == 0){
-                c++;
-                
-                if(c == k)
+                --k;
+                if(k == 0)
                     return i;
             }
         }
         
+        for(int i=(int)pow(n,0.5);i>=1;i--){
+            if(n%i == 0){
+                --k;
+                if(k == 0)
+                    return n/i;
+            }
+            
+        }
         return -1;
+  
     }
 };
