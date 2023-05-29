4acba7986c3fe5a185fdc28215af01b420de4b1b
Geeky-5hubham <81162491+Geeky-5hubham@users.noreply.github.com>
2023-05-30 02:38:12 +0530

    Time: 4 ms (77.75%), Space: 8.1 MB (49.26%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> v;
+        
+        if(s.size() == 1){
+            if(s == " ")
+                return "";
+            
+            else
+                return s;
+        }
+        
+        string temp = "";
+        for(int i=0;i<s.size();i++){
+            if(s[i] != ' '){
+                temp += s[i];
+            }
+            
+            else if(s[i] == ' '){
+                if(temp.size() > 0){
+                    v.push_back(temp);
+                    temp = "";
+                }
+            }
+        }
+        
+        if(temp.size() > 0)
+            v.push_back(temp);
+        
+        string ans = "";
+        
+        reverse(v.begin(),v.end());
+        
+        for(int i=0;i<v.size();i++){
+            ans += v[i];
+            ans += ' ';
+        }
+        
+        ans.erase(ans.begin()+ans.size()-1);
+        
+        return ans;
+    }
+};
