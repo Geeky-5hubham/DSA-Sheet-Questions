4cebdab8b4242f6e7619d1437ebf21462d86a904
Geeky-5hubham <81162491+Geeky-5hubham@users.noreply.github.com>
2023-07-01 02:10:16 +0530

    Time: 9 ms (49.44%), Space: 10.7 MB (86.87%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,47 +1,37 @@
 class Solution {
 public:
-    
-    vector<vector<int>> ans; // 2 D vector to store our answer
-    void solve(int i, vector<int>& arr, vector<int>& temp, int target)
+    void solve(int i,int target,vector<int> &candidates,vector<vector<int>> &ans,vector<int> &ds)
     {
-        // if our target becomes zero at any point, then yess!! we wil find a possible combination
-        if(target == 0) 
-        {
-            ans.push_back(temp); // include that combination in our answer
-            return; // and then return, we are now not gonna explore more possiblity
+        int n = candidates.size();
+        
+        if(i == n){
+            if(target == 0)
+                ans.push_back(ds);
+            
+            return;
         }
         
-        // if at any point target becomes less than zero, then simply return, saying that no it is notpossible to our target combination sum
-        if(target < 0)
-            return;
+        if(candidates[i] <= target){
+            
+            ds.push_back(candidates[i]);
+            solve(i,target-candidates[i],candidates,ans,ds);
+            ds.pop_back();
+          
+        }
         
-        // if index crosses the last index, we will return saying that no more element is left to choosee
-        if(i == arr.size())
-            return;
+
+        solve(i+1,target,candidates,ans,ds);
         
-        // As we dicussed for every element we have two choices whether to include in our answer or not include in our answer. 
-		//so now, we are doing that
         
-        // we are not taking the ith element,
-        // so without decreasing sum we will move to next index because it will not contribute in making our sum
-        solve(i + 1, arr, temp, target);
         
-        // we are taking the ith element and not moving onto the next element because it may be possible that this element again contribute in making our sum.
-        // but we decrease our target sum as we are consediring that this will help us in making our target sum
-        
-        temp.push_back(arr[i]); // including ith element
-        solve(i, arr, temp, target - arr[i]); // decreasing sum,and call again function
-        temp.pop_back(); // backtrack
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int > > ans;
+        vector<int> ds;
         
-        ans.clear(); // clear global array, make to sure that no garbage value is present in it
+        solve(0,target,candidates,ans,ds);
         
-        vector<int> temp; // temporary vector that tries all possible combination
-        
-        solve(0, candidates, temp, target); // calling function, and see we start from index zero
-        
-        return ans; // finally return the answer array
+        return ans;
     }
 };
