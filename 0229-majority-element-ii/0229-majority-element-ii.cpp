bf2a8d0ec9936e5dab314f57cc613c472da7290e
Shubham Sinha <81162491+Geeky-5hubham@users.noreply.github.com>
2023-10-05 23:51:08 +0530

    Time: 11 ms (51.76%), Space: 16.3 MB (18.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        
-        vector<int> ans;
-        set<int> s;
-        
-        if(nums.size() == 1)    return {nums[0]};
-        
-        if(nums.size() == 2)
-        {
-            if(nums[0] == nums[1])  return {nums[0]};
-            else    return nums;
-            
+        // Checks for smaller cases
+        // Checking sizes 1 and 2 because they are less than 3
+        if (nums.size() == 1) return nums;
+        if (nums.size() == 2) {
+            if (nums[0] == nums[1]) return { nums[0] };
+            return nums;
         }
-        int count = 0;
-        sort(nums.begin(),nums.end());
-        
-        for(int i=0;i<nums.size()-1;i++)
-        {
-            if(nums[i] == nums[i+1]){
-                count++;
-                
-                if(count+1 > nums.size()/3)     s.insert(nums[i]);
+
+        // Modified Boyer-Moore Voting
+        int count1 = 0;
+        int count2 = 0;
+        int candidate1;
+        int candidate2;
+
+        for (const auto num : nums) {
+            if      (count1 == 0 && num != candidate2) candidate1 = num;
+            else if (count2 == 0 && num != candidate1) candidate2 = num;
+            
+            if      (num == candidate1) ++count1;
+            else if (num == candidate2) ++count2;
+            else {
+                --count1;
+                --count2;
             }
-            
-            else    count = 0;
         }
-       
-        for(auto i:s)
-        {
-            ans.push_back(i);
-        }
-        return ans;
+
+        // Confirmation passes
+        int freq1 = 0;
+        int freq2 = 0;
+        for (const auto num : nums) 
+            if      (num == candidate1) ++freq1;
+            else if (num == candidate2) ++freq2;
+
+        vector<int> majorityElements;
+        if (freq1 > nums.size() / 3) majorityElements.emplace_back(candidate1);
+        if (freq2 > nums.size() / 3) majorityElements.emplace_back(candidate2);
+
+        return majorityElements;
     }
 };
